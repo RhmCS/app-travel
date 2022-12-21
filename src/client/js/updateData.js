6806701eb93ffad9e4d02c1a63b8cf170afe5c41
Reham Saud <49171601+RhmCS@users.noreply.github.com>
2022-12-21 14:31:35 +0300

    Update updateData.js
    Deleted an extra step to fetch data.

--- src/client/js/updateData.js
+++ src/client/js/updateData.js
@@ -2,30 +2,24 @@ const today = new Date();
 
 // Function to Retrieve Data
 async function updateData() {
-  const request = await fetch("/");
-  console.log(request);
-  try {
-    // Transform into JSON
-    const allData = await request.json();
-    console.log("allData", allData);
+   try {
     // Write updated data to DOM elements
     const tripdetiles = document.querySelector(".trip-detiles");
-
-    if (allData.date == today) {
+    if (data.date == today) {
       tripdetiles.innerHTML = ` <div class="trip">
         <div id="photo">
-        <img class="trip-img" src="${allData.photo}" alt="${allData.city} "></div>
-        <div id="cityName">Trip to : ${allData.city}</div>
-        <div id="todate">Leaving date is :  ${allData.date}</div>
-        <div id="weather">The weather will be ${allData.weather_des},${allData.temp}&deg;C degree</div>
+        <img class="trip-img" src="${data.photo}" alt="${data.city} "></div>
+        <div id="cityName">Trip to : ${data.city}</div>
+        <div id="todate">Leaving date is :  ${data.date}</div>
+        <div id="weather">The weather will be ${data.weather_des},${data.temp}&deg;C degree</div>
         </div>`;
     } else {
       tripdetiles.innerHTML = `<div class="trip">
         <div id="photo">
-        <img class="trip-img" src="${allData.photo}" alt="${allData.city} "></div>
-        <div id="cityName">Trip to : ${allData.city}</div>
-        <div id="todate">Leaving date is :  ${allData.date}</div>
-        <div id="weather"> Weather forecast is ${allData.weather_des},${allData.temp}&deg;C degree</div>
+        <img class="trip-img" src="${data.photo}" alt="${data.city} "></div>
+        <div id="cityName">Trip to : ${data.city}</div>
+        <div id="todate">Leaving date is :  ${data.date}</div>
+        <div id="weather"> Weather forecast is ${data.weather_des},${data.temp}&deg;C degree</div>
         </div>`;
     }
   } catch (error) {
